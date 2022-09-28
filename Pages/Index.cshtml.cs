abec674243f87bdd9b286720c7954a750c665570
CharlesOK <okwechimeamechi@gmail.com>
2022-09-28 01:12:55 -0400

    added page model class properties

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -7,6 +7,14 @@ public class IndexModel : PageModel
 {
   private readonly ILogger<IndexModel> _logger;
 
+  [BindProperty]
+  public double FirstNumber { get; set; }
+  [BindProperty]
+  public double SecondNumber { get; set; }
+
+  [BindProperty]
+  public double ThirdNumber { get; set; }
+
   public IndexModel(ILogger<IndexModel> logger)
   {
     _logger = logger;
