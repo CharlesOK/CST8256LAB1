383f94f5320b19849f876a3530f74804c8d399e4
CharlesOK <okwechimeamechi@gmail.com>
2022-09-28 13:26:39 -0400

    updated post method to handle form submission

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+// using System.Linq;
+// using System.Collections.Generic;
+
 
 namespace Lab1.Pages;
 
@@ -8,12 +11,23 @@ public class IndexModel : PageModel
   private readonly ILogger<IndexModel> _logger;
 
   [BindProperty]
-  public string FirstNumber { get; set; }
+  public string? FirstNumber { get; set; }
   [BindProperty]
-  public string SecondNumber { get; set; }
+  public string? SecondNumber { get; set; }
 
   [BindProperty]
-  public string ThirdNumber { get; set; }
+  public string? ThirdNumber { get; set; }
+
+  public string? ErrorMessage { get; set; }
+  public string? Min { get; set; }
+
+  public string? Max { get; set; }
+  public string? Total { get; set; }
+
+  public string? Average { get; set; }
+
+  public int Count { get; set; }
+  public bool Display { get; set; } = false;
 
   public IndexModel(ILogger<IndexModel> logger)
   {
@@ -24,5 +38,50 @@ public class IndexModel : PageModel
   {
 
   }
+
+  public void OnPost()
+  {
+
+    var numbersList = new List<double>();
+
+    double numbers;
+
+
+    if (double.TryParse(FirstNumber, out numbers))
+    {
+      numbersList.Add(numbers);
+    }
+
+
+    if (double.TryParse(SecondNumber, out numbers))
+    {
+      numbersList.Add(numbers);
+    }
+
+
+    if (double.TryParse(ThirdNumber, out numbers))
+    {
+      numbersList.Add(numbers);
+    }
+
+    if (numbersList.Count == 0)
+    {
+      ErrorMessage = "You did not enter any number, no statistics were calculated!";
+      return;
+    }
+
+
+    Count = numbersList.Count;
+    Max = numbersList.Max().ToString("F2");
+    Min = numbersList.Min().ToString("F2");
+    Average = numbersList.Average().ToString("F2");
+    Total = numbersList.Sum().ToString("F2");
+    Display = true;
+
+
+
+
+  }
+
 
 }
